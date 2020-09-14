877d326471e1f82b36eb7b0b99e4d6412013d154
R1m7PY <59962959+R1m7PY@users.noreply.github.com>
2020-09-14 17:06:12 +0500

    Add files via upload

---
+++ project_index_v.2.5.ino
@@ -0,0 +1,73 @@
+#include <Servo.h> // библиотека для серво
+
+//--создаем объекты--//
+Servo hvost_d; // первый сервопривод
+Servo hvost_b; // второй сервопривод
+Servo last_r; // серво для правого ласта
+Servo last_l; // серво для левого ласта
+//-------------------//
+
+int i; // переменная для вращения хвоста
+int a; // переменная для вращения ласт
+
+
+void setup() {
+//--подключение серво к пинам--//
+  hvost_d.attach(7);
+  hvost_b.attach(8);
+  last_r.attach(12);
+  last_l.attach(4);
+//-----------------------------//
+  /* питание от внешнего блока питания */
+  
+  hvost_d.write(90);
+  hvost_b.write(90);
+  last_r.write(90);
+  last_l.write(90);
+  a = 90;
+  
+}
+
+void loop() {
+
+  for (i = -15; i <= 15; i++) {
+    delay(30);
+    hvost_b.write(90 + i);
+    last_r.write(a + i);
+    last_l.write(a - i);
+  }
+  
+   for (i = 15; i >= -15; i--) {
+    delay(30);
+    hvost_d.write(90 - i);
+    last_r.write(a + i);
+    last_l.write(a - i);
+  }
+  
+   for (i = -15; i <= 15; i++) {
+    delay(30);
+    hvost_b.write(90 - i);
+    last_r.write(a + i);
+    last_l.write(a - i);
+  }
+  
+  for (i = 15; i >= -15; i--) {
+    delay(30);
+    hvost_d.write(90 + i);
+    last_r.write(a + i);
+    last_l.write(a - i);
+  }
+/*
+  for (i = 0; i <= 15; i++) {
+    last_r.write(90 + i);
+    last_l.write(90 - i);
+    delay(20);
+  }
+  
+  for (i = 0; i <= 15; i++) {
+    last_r.write(105 - i);
+    last_l.write(75 + i);
+    delay(20);
+  }
+  */
+}
